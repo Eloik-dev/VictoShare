3352552881f7b3e878e069ada88598eedb78e1e8
Eloik-dev <eloik.rousseau@gmail.com>
2025-07-31 16:40:42 -0400

    Update

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,10 @@ import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
+  resolve: {
+    alias: [
+      { find: '@', replacement: path.resolve(__dirname, 'src') }
+    ]
+  },
   plugins: [react(), tsconfigPaths()]
 });
